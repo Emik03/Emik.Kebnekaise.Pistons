7d672061514b5f6774a5ff94a039f94c05222378
Emik <emik.lakunishok@gmail.com>
2023-01-18 05:06:15 +0100

    Use SPDX short-form identifiers

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,18 +1,7 @@
-#region Emik.MPL
-
-// <copyright file="GlobalUsings.cs" company="Emik">
-// Copyright (c) Emik. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-// </copyright>
-
-#endregion
-
-#region
-
+// SPDX-License-Identifier: MPL-2.0
 global using Celeste;
 global using Celeste.Mod;
 global using Celeste.Mod.Entities;
 global using Microsoft.Xna.Framework;
 global using Monocle;
 global using static Monocle.Ease;
-
-#endregion
